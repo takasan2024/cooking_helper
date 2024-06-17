e1a8823057148df4a71208f3a9284ff7558fea9a
takasan2024 <gyhuijk7896@yahoo.co.jp>
2024-06-17 20:51:41 +0900

    ログアウト状態では投稿できない

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -1,5 +1,6 @@
 class CookingsController < ApplicationController
   before_action :set_cooking, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
   
   def index
     @cookings = Cooking.all
@@ -36,5 +37,15 @@ class CookingsController < ApplicationController
 
   def cooking_params
     params.require(:cooking).permit(:name, :image, :text)
+  end
+
+  def set_cooking
+    @cooking = Cooking.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
